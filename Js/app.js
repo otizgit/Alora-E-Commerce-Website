58b94286e91defad2f8fdc90503062cd6d3047fb
Emmanuel <emmanueljoe777@gmail.com>
2023-04-01 16:52:27 +0100

    update

--- Js/app.js
+++ Js/app.js
@@ -43,25 +43,25 @@ list.addEventListener("click", function (e) {
   }
 });
 
-// const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-// tl.to(".intro-image", { y: "0%", duration: 0.5, stagger: 0.25 });
-// tl.to(".slider", { y: "-100%", duration: 1.2, delay: 0.3 });
-// tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-// tl.fromTo(".logo", { opacity: 0 }, { opacity: 1, duration: 1 });
-// tl.fromTo(".to-top", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-// tl.fromTo(".top-nav", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-// tl.fromTo(".hero-content", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-// tl.fromTo(".hero-content", { y: "10%" }, { y: "0%", duration: 1 }, "-=1");
-// tl.fromTo(".about-content", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-// tl.fromTo(".about-content", { y: "10%" }, { y: "0%", duration: 1 }, "-=1");
-// tl.fromTo(".login-container", { y: "10%" }, { y: "0%", duration: 1 }, "-=1");
-// tl.fromTo(
-//   ".login-container",
-//   { opacity: 0 },
-//   { opacity: 1, duration: 1 },
-//   "-=1"
-// );
+tl.to(".intro-image", { y: "0%", duration: 0.5, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.2, delay: 0.3 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo(".logo", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".to-top", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".top-nav", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".hero-content", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".hero-content", { y: "10%" }, { y: "0%", duration: 1 }, "-=1");
+tl.fromTo(".about-content", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".about-content", { y: "10%" }, { y: "0%", duration: 1 }, "-=1");
+tl.fromTo(".login-container", { y: "10%" }, { y: "0%", duration: 1 }, "-=1");
+tl.fromTo(
+  ".login-container",
+  { opacity: 0 },
+  { opacity: 1, duration: 1 },
+  "-=1"
+);
 
 AOS.init({
   duration: 1000,
